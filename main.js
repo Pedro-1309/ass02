719a2094ec3c1799574ac4b8c3a0a6821aec3c79
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2024-05-04 11:00:32 +0200

    scrape now searches for occurrencies on current page

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function onSubmit() {
     document.getElementById("startButton").setAttribute("disabled", true)
     console.log("submitted")
-    scrape("https://www.op.gg/", "Draven", 3, 0)
+    scrape(
+        document.getElementById("inputUrl").value,
+        document.getElementById("word").value,
+        document.getElementById("depth").value,
+        0
+    )
 }
 
 function onStop() {
@@ -10,9 +15,28 @@ function onStop() {
 }
 
 function scrape(url, word, maxDepth, currentDepth) {
-    console.log("scraping: " + url)
-    fetch(url)
-        .then((response) => console.log(response.body.getReader().toString()));
+    fetch(url).then(async (response) => {
+        if (!response.ok)
+            await updatePage(" Couldn't connet to page: " + url)
+        else {
+            var text = await response.text()
+            console.log("scraping: " + text + "\n word: " + word + "\n depth: " + currentDepth)
+            var index = text.indexOf(word)
+            var count = 1
+            while (index != -1) {
+                console.log(index)
+                updatePage(" Occurrence " + count + " of word " + word + " in url " + url)
+                text = text.replace(word, "")
+                index = text.indexOf(word)
+                count++
+            }
+        }
+    });
+}
 
-    return true
+async function updatePage(value)  {
+    var ul = document.getElementById("results");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(value));
+    ul.appendChild(li);
 }
