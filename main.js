7514db638e38c7e428d6783c58033c4bbe4d0530
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2024-05-04 11:05:25 +0200

    removed awaits

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function onSubmit() {
     document.getElementById("startButton").setAttribute("disabled", true)
     console.log("submitted")
-    scrape(
-        document.getElementById("inputUrl").value,
-        document.getElementById("word").value,
-        document.getElementById("depth").value,
-        0
-    )
+    if (document.getElementById("word").value != "" &&
+        document.getElementById("depth").value >= 0)
+        scrape(
+            document.getElementById("inputUrl").value,
+            document.getElementById("word").value,
+            document.getElementById("depth").value,
+            0
+        )
 }
 
 function onStop() {
@@ -15,26 +17,27 @@ function onStop() {
 }
 
 function scrape(url, word, maxDepth, currentDepth) {
-    fetch(url).then(async (response) => {
+    fetch(url).then((response) => {
         if (!response.ok)
-            await updatePage(" Couldn't connet to page: " + url)
+            updatePage(" Couldn't connet to page: " + url)
         else {
-            var text = await response.text()
-            console.log("scraping: " + text + "\n word: " + word + "\n depth: " + currentDepth)
-            var index = text.indexOf(word)
-            var count = 1
-            while (index != -1) {
-                console.log(index)
-                updatePage(" Occurrence " + count + " of word " + word + " in url " + url)
-                text = text.replace(word, "")
-                index = text.indexOf(word)
-                count++
-            }
+            response.text().then(text => {
+                console.log("scraping: " + text + "\n word: " + word + "\n depth: " + currentDepth)
+                var index = text.indexOf(word)
+                var count = 1
+                while (index != -1) {
+                    console.log(index)
+                    updatePage(" Occurrence " + count + " of word " + word + " in url " + url)
+                    text = text.replace(word, "")
+                    index = text.indexOf(word)
+                    count++
+                }
+            })
         }
     });
 }
 
-async function updatePage(value)  {
+function updatePage(value)  {
     var ul = document.getElementById("results");
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(value));
